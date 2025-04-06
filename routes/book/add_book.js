3520b4f7feb06612ead76f13200681717ab6de1c
Gutsalov Artyom <aguczalov@mail.ru>
2025-04-06 16:04:17 +0300

    add gitignore

--- routes/book/add_book.js
+++ routes/book/add_book.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const pool = require('../../database'); // Подключение к базе данных
+const pool = require('../../database');
 const { resetAuthorsSequence } = require('../../utils/reorderAuthors');
 const { resetLiteratureSequence } = require('../../utils/reorderLiterature');
 
